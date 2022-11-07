d91af11589b0c2e9fafb97a73971eb724ca6d7e3
Jennifer Goijman <jennifer.goijman@dcsl.com>
2022-11-07 16:48:23 +0700

    chore: add .eslint

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>Editor Version Widget</p>
+    </header>
+  </div>
+);
+
+export default App;
